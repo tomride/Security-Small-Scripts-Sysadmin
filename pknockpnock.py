c2bc5b80bac459022b84f0884aa8b39271857872
tomride <tomride@protonmail.com>
2017-07-10 17:03:07 +0200

    Add files via upload

---
+++ pknockpnock.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import socket
+
+#SIMPLE SCRIPT TO KNOCK PORTS TO OPEN SSH server
+
+
+ippaddress = raw_input("IP server to knock knock")
+puertos = input("list of ports separated by commas:")
+
+
+for x in puertos:
+    try:
+        conexion = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        conexion.connect((ippaddress,x))
+    except:
+        print("ok")
+
+print("Try to connect to server")
